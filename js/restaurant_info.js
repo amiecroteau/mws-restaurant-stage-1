ee9a23d9272568a2cdeda6a7007e4db811bcb5b9
Amie Croteau <ac04400@ravens.racc.edu>
2018-09-25 20:34:47 -0400

    testing css functionality

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYW1pZWNyb3RlYXUiLCJhIjoiY2ptaDA1MnA4Mjh1eTNrcjFrNTNxamVkbyJ9.NdW3Z_vjWZj6CrF0qZjPIQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
